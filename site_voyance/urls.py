8f1346852940701463f28d54afce0def46fa173f
nardine2 <124087670+nardine2@users.noreply.github.com>
2023-02-02 06:42:30 +1300

    Add files via upload

--- site_voyance/urls.py
+++ site_voyance/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.conf.urls import url, include
 from . import views
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.home),
@@ -29,3 +30,8 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+
+
